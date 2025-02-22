370197a4042c6014a4b9e5fced5b16ff8083c9a4
ali-raza-400 <aliraza.12748g@gmail.com>
2025-02-22 21:24:08 +0500

    ts issue

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -30,7 +30,7 @@ import { yupResolver } from '@hookform/resolvers/yup'
 
 // ** Hooks
 import { useAuth } from 'src/hooks/useAuth'
-import useBgColor from 'src/@core/hooks/useBgColor'
+// import useBgColor from 'src/@core/hooks/useBgColor'
 import { useSettings } from 'src/@core/hooks/useSettings'
 
 // ** Configs
@@ -102,7 +102,7 @@ const LoginPage = () => {
   // ** Hooks
   const auth = useAuth()
   const theme = useTheme()
-  const bgColors = useBgColor()
+  // const bgColors = useBgColor()
   const { settings } = useSettings()
   const hidden = useMediaQuery(theme.breakpoints.down('md'))
 
